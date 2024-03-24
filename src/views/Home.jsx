77ab6cb73394d4a25a36b3930421dce254c3c368
Pacifista00 <adamsamudera99@gmail.com>
2024-03-24 23:17:58 +0700

    membuat git reset

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,11 @@
 import HeroImage from "../components/HeroImage";
+import TopGame from "../components/TopGame";
 
 const Home = () => {
   return (
     <>
       <HeroImage />
+      <TopGame />
       <div className="px-4">
         <h1 className="object-cover">Hello Gamers!!</h1>
       </div>
