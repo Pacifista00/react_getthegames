ca144e9a49a4c765ac9fafd13f6d17444c2109bc
Pacifista00 <adamsamudera99@gmail.com>
2024-04-24 15:38:09 +0700

    memperbaiki halaman games

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -1,5 +1,6 @@
 import axiosInstance from "../lib/axios";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const GameList = () => {
   const [data, setData] = useState(null);
@@ -22,20 +23,25 @@ const GameList = () => {
         {data ? (
           <div className="game-card grid grid-cols-2 md:grid-cols-4 gap-2">
             {data.map((game) => (
-              <figure className="relative group overflow-hidden mb-3 md:mb-0 cursor-pointer">
+              <figure
+                key={game.id}
+                className="relative group overflow-hidden mb-3 md:mb-0 cursor-pointer"
+              >
                 <img
                   className="w-full h-80 object-cover rounded-xl transition duration-300 group-hover:brightness-50"
                   src={game.image_path}
                   alt=""
                 />
-                <div className="md:absolute text-lg text-blue-800 md:text-slate-200 bottom-3 left-3 mt-2 transition duration-300 md:opacity-0 md:group-hover:opacity-100 md:-translate-x-7 group-hover:translate-x-0">
-                  <figcaption className="lg:text-2xl mb-1">
-                    {game.name}
-                  </figcaption>
-                  <button className="hidden md:block rounded-full md:outline hover:outline-none transition duration-300 bg-green-500 md:bg-transparent py-1 px-3 text-slate-200 hover:bg-green-500 active:bg-green-700">
-                    Check
-                  </button>
-                </div>
+                <Link to={`/product/game/${game.id}`}>
+                  <div className="md:absolute text-lg text-slate-700 md:text-slate-200 bottom-3 left-3 mt-2 transition duration-300 md:opacity-0 md:group-hover:opacity-100 md:-translate-x-7 group-hover:translate-x-0">
+                    <figcaption className="lg:text-2xl mb-1">
+                      {game.name}
+                    </figcaption>
+                    <button className="hidden md:block rounded-full md:outline hover:outline-none transition duration-300 bg-green-500 md:bg-transparent py-1 px-5 text-slate-200 hover:bg-green-500 active:bg-green-700">
+                      Check
+                    </button>
+                  </div>
+                </Link>
               </figure>
             ))}
           </div>
